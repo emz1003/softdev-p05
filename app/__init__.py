7723c7a2162963edd9f599f02b34ce2b1d561cd4
Kevin Li <kevinli4321@gmail.com>
2020-06-05 07:51:41 -0400

    C'mon pytest give me a break

--- app/__init__.py
+++ app/__init__.py
@@ -16,15 +16,66 @@ SCOPES = [
     'https://www.googleapis.com/auth/classroom.profile.photos'
 ]
 
-app = Flask(__name__)
-# Secret key handling ========================================
-# To team: run str(os.urandom(32)) to get your own, and paste it into a secret_key.txt
-# file in the app/ directory.
-SECRET_KEY_FILE = 'secret_key.txt'
-file = open(SECRET_KEY_FILE, 'r')
-app.secret_key = file.read()
-file.close()
-# ============================================================
+# Flask app ====================================================================
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = os.getenv('PASSPHRASE', 'Optional default value')
+
+    @app.route("/")
+    def home():
+        if 'credentials' not in session:
+            return render_template("login.html")
+
+        credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+        gclass = build('classroom', 'v1', credentials=credentials)
+
+        # Call the Classroom API
+        results = gclass.courses().list(pageSize=20).execute()
+        courses = results.get('courses', [])
+        userinfo = get_curruserinfo(credentials)
+
+        session['credentials'] = credentials_to_dict(credentials)
+
+        return render_template("classes.html", courses = courses, userinfo = userinfo)
+
+    @app.route("/auth")
+    def auth():
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRET_FILE, scopes=SCOPES)
+
+        flow.redirect_uri = url_for('callback', _external = True)
+        auth_url, state = flow.authorization_url(
+            access_type = 'offline',
+            prompt = 'consent',
+            include_granted_scopes = 'true')
+
+        session['state'] = state
+
+        return redirect(auth_url)
+
+    @app.route("/callback")
+    def callback():
+        state = session['state']
+
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            CLIENT_SECRET_FILE, scopes=SCOPES, state = state)
+        flow.redirect_uri = url_for('callback', _external = True)
+
+        auth_response = request.url
+        flow.fetch_token(authorization_response = auth_response)
+        credentials = flow.credentials
+        session['credentials'] = credentials_to_dict(credentials)
+
+        return redirect(url_for('home'))
+
+    @app.route("/logout")
+    def logout():
+        session.pop('state')
+        session.pop('credentials')
+        return redirect(url_for('home'))
+
+    return app
+# ==============================================================================
 
 def credentials_to_dict(credentials):
   return {'token': credentials.token,
@@ -46,61 +97,8 @@ def get_curruserinfo(credentials):
 
     return dict
 
-# Routes =====================================================
-@app.route("/")
-def home():
-    if 'credentials' not in session:
-        return render_template("login.html")
-
-    credentials = google.oauth2.credentials.Credentials(**session['credentials'])
-    gclass = build('classroom', 'v1', credentials=credentials)
-
-    # Call the Classroom API
-    results = gclass.courses().list(pageSize=20).execute()
-    courses = results.get('courses', [])
-    userinfo = get_curruserinfo(credentials)
-
-    session['credentials'] = credentials_to_dict(credentials)
-
-    return render_template("classes.html", courses = courses, userinfo = userinfo)
-
-@app.route("/auth")
-def auth():
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRET_FILE, scopes=SCOPES)
-
-    flow.redirect_uri = url_for('callback', _external = True)
-    auth_url, state = flow.authorization_url(
-        access_type = 'offline',
-        prompt = 'consent',
-        include_granted_scopes = 'true')
-
-    session['state'] = state
-
-    return redirect(auth_url)
-
-@app.route("/callback")
-def callback():
-    state = session['state']
-
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRET_FILE, scopes=SCOPES, state = state)
-    flow.redirect_uri = url_for('callback', _external = True)
-
-    auth_response = request.url
-    flow.fetch_token(authorization_response = auth_response)
-    credentials = flow.credentials
-    session['credentials'] = credentials_to_dict(credentials)
-
-    return redirect(url_for('home'))
-
-@app.route("/logout")
-def logout():
-    session.pop('state')
-    session.pop('credentials')
-    return redirect(url_for('home'))
-
 if __name__ == "__main__":
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
+    app = create_app()
     app.debug = True
     app.run()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from flask import Flask
-from app import app
+from app import create_app
 
 @pytest.fixture
 def app():
-    app = Flask(__name__)
+    app = create_app()
     app.debug = True
     return app
